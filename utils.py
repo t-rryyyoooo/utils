2c45d81692f963de1c4ee47abcbc544de3f8a996
zerebom <ice.choco.pudding.kokoro@gmail.comm>
2021-04-09 12:42:31 +0000

    Set seed

--- utils.py
+++ utils.py
@@ -1,9 +1,21 @@
 import numpy as np
 import SimpleITK as sitk
+import random
+import torch
+import os
 from decimal import Decimal, ROUND_HALF_UP
 import re
 import requests
 from secret import token
+
+def setSeed(seed: int = 42) -> None:
+    np.random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
 
 def isMasked(img_or_array):
     if isinstance(img_or_array, sitk.Image):
