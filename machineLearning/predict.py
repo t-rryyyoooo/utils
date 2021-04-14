ec1ed8cb67773db76c946d6ab0c06e9379036b29
zerebom <ice.choco.pudding.kokoro@gmail.comm>
2021-04-14 06:56:35 +0000

    R

--- machineLearning/predict.py
+++ machineLearning/predict.py
@@ -5,11 +5,16 @@ class Predictor():
         self.model = model
         self.device = device
 
-    def __call__(self, input_array_list):
+    def __call__(self, input_array_or_list):
         """
-        image_array = [input_array_1, input_array_2, ...]
+        input_array_or_list -- np.ndarray or [np.ndarray, np.ndarray]
 
         """
+        if isinstance(input_array_or_list, np.ndarray):
+            input_array_list = [input_array_or_list]
+        else:
+            input_array_list = input_array_or_list
+
         input_array_list = [torch.from_numpy(input_array).to(self.device, dtype=torch.float) for input_array in input_array_list]
 
         segmented_array = self.model(*input_array_list)
