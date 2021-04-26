3f1183f3307e693bf7ef2751a1def7eb949031a0
Tanimoto Ryo <rryyyoooo@i.softbank.jp>
2021-04-26 20:08:37 +0900

    R

--- patchGenerator/slicePatchGenerator.py
+++ patchGenerator/slicePatchGenerator.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def SlicePatchGenerator():
+class SlicePatchGenerator():
     def __init__(self, image_array=None, patch_width=1, slide=None, axis=0):
         """ Slice image array along axis.
 
@@ -18,7 +18,6 @@ def SlicePatchGenerator():
         self.axis        = axis
         self.plane_size  = self.getPlaneSize(image_array.shape, axis)
 
-        assert (image_array.shape[axis] % patch_width) == 0
 
         max_size = image_array.shape[axis] - patch_width + 1
         self.indices = [ i for i in range(0, max_size, slide)]
@@ -28,7 +27,8 @@ def SlicePatchGenerator():
 
     def __call__(self):
         for index in self.indices:
-            slices = tuple(np.insert(self.plane_size, self.axis, index))
+            slices = [slice(0, s) for s in self.plane_size]
+            slices = tuple(np.insert(slices, self.axis, slice(index, index + self.patch_width)))
 
             sliced_image_array = self.image_array[slices]
 
