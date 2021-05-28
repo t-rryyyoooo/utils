bbec69e550d9a91af27f62925a527e95738ec941
t-rryyyoooo <rryyyoooo@i.softbank.jp>
2021-05-28 17:37:50 +0900

    R

--- imageProcessing/perimeterLabelArrayCreater.py
+++ imageProcessing/perimeterLabelArrayCreater.py
@@ -7,10 +7,10 @@ class PerimeterLabelArrayCreater():
         self.num_class   = num_class
 
     def __call__(self, label_array):
-        onehot_array = self.makeOnehot(label_array, args.num_class)
+        onehot_array = self.makeOnehot(label_array, self.num_class)
 
         perimeter_array = None
-        for c in range(args.num_class):
+        for c in range(self.num_class):
             pa = self.countAmbientLabel(onehot_array[..., c], radius=self.radius)
             pa = pa[np.newaxis, ...]
 
@@ -18,7 +18,7 @@ class PerimeterLabelArrayCreater():
                 perimeter_array = pa
             else:
                 perimeter_array = np.concatenate([perimeter_array, pa], axis=0)
-        counter_array = self.countAmbientLabel(np.ones_like(label_array), radius=args.radius)
+        counter_array = self.countAmbientLabel(np.ones_like(label_array), radius=self.radius)
 
         perimeter_array /= counter_array
 
